c40de229330c1b3e27902b1c5fb3c3ed60aa3773
Ouriel91 <orieloh91@gmail.com>
2022-05-28 22:10:07 +0300

    feat adds complete workshop 6 without bonus

--- weather.js
+++ weather.js
@@ -1,7 +1,7 @@
 // Recommended order for your solution:
-// 1. Install the dotenv package.
-// 2. Add a dotenv file, put the API key in dotenv and print it.
-// 3. Install the node-fetch package.
+// 1. Install the dotenv package. //
+// 2. Add a dotenv file, put the API key in dotenv and print it. //
+// 3. Install the node-fetch package. //
 // 4. Create a method that calls the API to get temperature using node-fetch.
 // 5. Install the commander package.
 // 6. Create a basic commander skeleton without the actions implementation (just the metadata and commands configuration).
@@ -12,22 +12,40 @@
 import chalk from "chalk";
 import { Command } from "commander";
 const program = new Command();
+import 'dotenv/config'
+import fetch from 'node-fetch'
+
+async function getTemprtature(city,options) {
+
+  let response
+
+  if (options.scale === "c"){
+    response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}&units=metric`)
+  }
+  else{
+    response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`)
+  }
+  
+  const data = await response.json();
+
+  return data.main.temp
+}
 
 program
-  .name("cli-calc")
-  .description("The best CLI calculator")
+  .name("weather")
+  .description("The best weather calculator")
   .version("1.0.0");
 
 program
-  .command("add")
-  .description("Add two numbers")
-  .argument("<number>", "first operand")
-  .argument("<number>", "second operand")
+  .command("get-temp")
+  .description("get temperature")
+  .argument("<string>", "give city temperature")
   .option("-c, --color <string>", "Result color", "white")
-  .action((firstNumber, secondNumber, options) => {
+  .option("-s, --scale <string>", "unix", "c")
+  .action(async(city, options) => {
     console.log(
       chalk[options.color](
-        `Result: ${Number(firstNumber) + Number(secondNumber)}`
+        `Result: ${await getTemprtature(city, options)}`
       )
     );
   });
